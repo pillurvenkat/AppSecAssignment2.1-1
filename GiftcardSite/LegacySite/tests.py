04f1733c9a3cd5d0cab1faf1872e246b6c24361f
pillurvenkat <32372068+pillurvenkat@users.noreply.github.com>
2020-12-22 03:00:30 -0500

    Regression test

--- GiftcardSite/LegacySite/tests.py
+++ GiftcardSite/LegacySite/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+class ViewsTestClass(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        #Run once to set up non-modified data for all class methods.
+        pass
+
+    def setUp(self):
+        #setUp: Run once for every test method to setup clean data.
+        pass
+
+    def test_register_view_good_data(self):
+        print("Method: test_false_is_false.")
+        self.assertFalse(False)
+
+    def test_register_view_xss_data(self):
+        print("Method: test_false_is_true.")
+        self.assertTrue(False)
+
+    def test_register_view_sqlInject_data(self):
+        print("Method: test_one_plus_one_equals_two.")
+        self.assertEqual(1 + 1, 2)
+		
+	def test_register_view_empty_data(self):
+        print("Method: test_one_plus_one_equals_two.")
+        self.assertEqual(1 + 1, 2)
+
+srf_client = Client(enforce_csrf_checks=True)
